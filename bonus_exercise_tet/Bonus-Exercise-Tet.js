b8d4729bf38e927a7fdbcaed4f213d1429ab433e
Nguyễn Trọng Hoàng <hoangnguyen30092003@gmail.com>
2024-02-07 16:19:37 +0700

    update_exercise_tet

---
+++ bonus_exercise_tet/Bonus-Exercise-Tet.js
@@ -0,0 +1,14 @@
+//toan tu ba ngoi
+const x = 100;
+const result = (x < 1000) ? console.log("nho hon 1000") : "lon hon hoac bang 1000";
+//shorthand evaluaated
+const variable2 = variable1 || ""
+//Variable declaration – If Comparison
+let x1, y, z = 3;
+if(isTurnOn);
+//For loop – For loop with decimal base
+//Object property
+const x2 = 1, y1 = 2;
+const obj = { x,y };
+//Rút gọn định nghĩa function(Sử dụng arrow function) 
+
